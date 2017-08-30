02fa20773f3b31c2766396f902965c3039ccc14e
Mrchen123456 <305883327@qq.com>
2017-08-30 15:37:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hp/testdemo/TestDemo1.java
@@ -0,0 +1,15 @@
+package com.example.hp.testdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.io.InputStreamReader;
+
+public class TestDemo1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_demo1);
+    }
+}
